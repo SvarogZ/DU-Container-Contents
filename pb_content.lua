01ae61ea3a73dd354ee5b5a5ab06cb7005a0d972
Svarog <alexander.s.zernov@gmail.com>
2022-05-05 16:30:01 +1200

    Create pb_content.lua

---
+++ pb_content.lua
@@ -0,0 +1,81 @@
+local update_time = 1 --export
+local hubMass = 55.8
+local screens = {}
+local core = nil
+local blinkIndicator = false
+
+local function initiateSlots()
+	for _, slot in pairs(unit) do
+		if type(slot) == "table" and type(slot.export) == "table" and slot.getElementClass then
+			local elementClass = slot.getElementClass():lower()
+			if elementClass:find("coreunit") then
+				core = slot
+			elseif elementClass == "screenunit" then
+				table.insert(screens,slot)
+			end
+		end
+	end
+	
+	if not core then
+		error("Core is not connected!")
+	end
+    
+	if #screens < 1 then
+		error("No screen connected!")
+	end
+	
+	table.sort(screens, function (a, b) return (a.getId() < b.getId()) end)
+end
+
+local function getDataFromScreens()
+	local dataFormScreens = {}
+	
+	for i,screen in ipairs(screens) do
+		dataFormScreens[i] = json.decode(screen.getScriptOutput())
+	end
+	
+	return dataFormScreens
+end
+
+initiateSlots()
+
+local dataFormScreens = getDataFromScreens()
+
+function update()
+	if blinkIndicator then blinkIndicator = false else blinkIndicator = true end
+	
+	for i,screen in ipairs(screens) do
+		if dataFormScreens[i] then
+			local dataToSend = {}
+			dataToSend.blinkIndicator = blinkIndicator
+
+			for _,id in pairs(dataFormScreens[i]) do
+				dataToSend[id] = core.getElementMassById(id) - hubMass
+			end
+
+			screen.setScriptInput(json.encode(dataToSend))
+		end 
+	end
+end
+
+function stop()
+	for i,screen in ipairs(screens) do
+		screen.setScriptInput("")
+	end
+end
+
+-------------------------
+-- UPDATE TIMER ---------
+-------------------------
+unit.setTimer("update", update_time)
+
+
+-------------------------
+-- FILTER UPDATE --------
+-------------------------
+update()
+
+-------------------------
+-- FILTER STOP ----------
+-------------------------
+stop()
