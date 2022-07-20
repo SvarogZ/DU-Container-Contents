789df80a13b6ec801ca32cfa0ce9e6257b38d24a
Svarog <alexander.s.zernov@gmail.com>
2022-07-20 13:22:38 +1200

    Create lable_render.lua

---
+++ lable_render.lua
@@ -0,0 +1,18 @@
+local name = "Cobalt"
+local imagePath = "resources_generated/env/voxel/ingots/cobalt-ingot/icons/env_cobalt-ingot_icon.png"
+
+local layer = createLayer()
+local rx, ry = getResolution()
+
+local image = loadImage (imagePath)
+local imageX, imageY = getImageSize (image)
+local imageRatio = imageY/imageX
+
+local preferedImageSizeY = ry / 2
+local preferedImageSizeX = preferedImageSizeY / imageRatio
+
+addImage (layer, image, (rx - preferedImageSizeX)/2, 0, preferedImageSizeX, preferedImageSizeY)
+
+local font = loadFont('FiraMono-Bold', ry/5)
+setNextTextAlign (layer, 1, 2)
+addText(layer, font, name, rx/2, ry*3/4)
