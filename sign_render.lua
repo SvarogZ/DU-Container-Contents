efc1414f7a5c9cdfc4402718bdc547071cfeae5b
Svarog <alexander.s.zernov@gmail.com>
2022-09-20 08:26:01 +1200

    Update sign_render.lua

--- sign_render.lua
+++ sign_render.lua
@@ -4,19 +4,19 @@ local imagePath = "resources_generated/env/voxel/ingots/cobalt-ingot/icons/env_c
 local layer = createLayer()
 local rx, ry = getResolution()
 
-local image = loadImage (imagePath)
-local imageX, imageY = getImageSize (image)
-local imageRatio = imageY/imageX
+local font = loadFont('FiraMono-Bold', ry/5)
+setNextTextAlign(layer, 1, 2)
+addText(layer, font, name, rx/2, ry*3/4)
 
-local preferedImageSizeY = ry / 2
-local preferedImageSizeX = preferedImageSizeY / imageRatio
+local image = loadImage(imagePath)
 
-if isImageLoaded (image) then
-    addImage (layer, image, (rx - preferedImageSizeX)/2, 0, preferedImageSizeX, preferedImageSizeY)
+if isImageLoaded(image) then
+    local imageX, imageY = getImageSize(image)
+    local imageRatio = imageY/imageX
+    local preferedImageSizeY = ry / 2
+    local preferedImageSizeX = preferedImageSizeY / imageRatio
+
+    addImage(layer, image, (rx - preferedImageSizeX)/2, 0, preferedImageSizeX, preferedImageSizeY)
 else
     requestAnimationFrame(15)
 end
-
-local font = loadFont('FiraMono-Bold', ry/5)
-setNextTextAlign (layer, 1, 2)
-addText(layer, font, name, rx/2, ry*3/4)
