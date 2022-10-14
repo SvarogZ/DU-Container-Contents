459af28e86eb77b4ce0c4adbab29a46f04520086
Svarog <alexander.s.zernov@gmail.com>
2022-10-15 09:26:09 +1300

    Update sign_render.lua

--- sign_render.lua
+++ sign_render.lua
@@ -1,18 +1,17 @@
-local name = "Cobalt"
-local imagePath = "resources_generated/env/voxel/ingots/cobalt-ingot/icons/env_cobalt-ingot_icon.png"
+local name = "Oxygen"
+local imagePath = "resources_generated/elements/pures/pure-gazs/pure-gaz-oxygen_001/icons/env_pure-gaz-oxygen_001_icon.png"
+local fontSize = 1
 
 local layer = createLayer()
 local rx, ry = getResolution()
 
-local font = loadFont('FiraMono-Bold', ry/5)
+local font = loadFont('FiraMono-Bold', ry/5*fontSize)
 setNextTextAlign(layer, 1, 2)
 addText(layer, font, name, rx/2, ry*3/4)
 
-if not image then
-	image = loadImage(imagePath)
-end
+image = loadImage(imagePath)
 
-if isImageLoaded(image) then
+if image and isImageLoaded and isImageLoaded(image) then
 	local imageX, imageY = getImageSize(image)
 	local imageRatio = imageY/imageX
 	local preferedImageSizeY = ry / 2
