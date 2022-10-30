84625009c855d0709061ab9573271735380ff9b1
Svarog <alexander.s.zernov@gmail.com>
2022-10-30 19:10:12 +1300

    Update sign_render_2.lua

--- sign_render_2.lua
+++ sign_render_2.lua
@@ -1,9 +1,10 @@
-local name = "A&S Fuel Tank M"
-local imagePath_1 = "resources_generated/elements/fueltanks/fueltank-atmospheric-radial_001_l/icons/env_fueltank-atmospheric-radial_001_l_icon.png"
-local imagePath_2 = "resources_generated/elements/fueltanks/fueltank-space-radial_001_l/icons/env_fueltank-space-radial_001_l_icon.png"
+local name = "B.Cont.S&M"
+local imagePath_1 = "resources_generated/elements/containers/container-legacy_001/icons/env_container-legacy_001_icon.png"
+local imagePath_2 = "resources_generated/elements/containers/container-legacy_001/icons/env_container-legacy_001_icon.png"
 
-local fontSize = 1.0
-local imageSIze = 1.4
+local fontSize = 1.2
+local imageSIze_1 = 1.0
+local imageSIze_2 = 1.5
 
 local layer = createLayer()
 local rx, ry = getResolution()
@@ -18,18 +19,19 @@ image_2 = loadImage(imagePath_2)
 if image_1 and isImageLoaded and isImageLoaded(image_1)
 	and image_2 and isImageLoaded and isImageLoaded(image_2) then
 
-	local preferedImageSizeY = ry / 2 * imageSIze
+	local preferedImageSizeY_1 = ry / 2 * imageSIze_1
+	local preferedImageSizeY_2 = ry / 2 * imageSIze_2
 
 	local imageX_1, imageY_1 = getImageSize(image_1)
 	local imageRatio_1 = imageY_1/imageX_1
-	local preferedImageSizeX_1 = preferedImageSizeY / imageRatio_1
+	local preferedImageSizeX_1 = preferedImageSizeY_1 / imageRatio_1
 
 	local imageX_2, imageY_2 = getImageSize(image_2)
 	local imageRatio_2 = imageY_2/imageX_2
-	local preferedImageSizeX_2 = preferedImageSizeY / imageRatio_2
+	local preferedImageSizeX_2 = preferedImageSizeY_2 / imageRatio_2
 
-	addImage(layer, image_1, rx/2 - preferedImageSizeX_1, 0, preferedImageSizeX_1, preferedImageSizeY)
-	addImage(layer, image_2, rx/2, 0, preferedImageSizeX_2, preferedImageSizeY)
+	addImage(layer, image_1, rx/4 - preferedImageSizeX_1/2, ry/4 - preferedImageSizeY_1/2, preferedImageSizeX_1, preferedImageSizeY_1)
+	addImage(layer, image_2, rx/4*3 - preferedImageSizeX_2/2, ry/4 - preferedImageSizeY_2/2, preferedImageSizeX_2, preferedImageSizeY_2)
 else
 	requestAnimationFrame(15)
 end
